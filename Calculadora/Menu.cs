c4105870907f265e40b0eb759cba3af3a628ee5c
Gabriel <jgerazo7@misena.edu.co>
2023-05-11 20:46:19 -0500

    creacion del menu

--- Calculadora/Menu.cs
+++ Calculadora/Menu.cs
@@ -1,12 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculadora
 {
-    internal class Menu
+    class Menu
     {
+        private string palabra1;
+        string palabra2;
+        long numero1;
+        long numero2;
+
+        public void Menucito()
+        {
+            Logica_Operacional llamados = new Logica_Operacional();
+
+            Console.WriteLine("************************************************");
+            Console.WriteLine("*╔══╗╔══╗╔╗──╔══╗╔╗╔╗╔╗──╔══╗╔══╗─╔══╗╔═══╗╔══╗*");
+            Console.WriteLine("*║╔═╝║╔╗║║║──║╔═╝║║║║║║──║╔╗║║╔╗╚╗║╔╗║║╔═╗║║╔╗║*");
+            Console.WriteLine("*║║──║╚╝║║║──║║──║║║║║║──║╚╝║║║╚╗║║║║║║╚═╝║║╚╝║*");
+            Console.WriteLine("*║║──║╔╗║║║──║║──║║║║║║──║╔╗║║║─║║║║║║║╔╗╔╝║╔╗║*");
+            Console.WriteLine("*║╚═╗║║║║║╚═╗║╚═╗║╚╝║║╚═╗║║║║║╚═╝║║╚╝║║║║║─║║║║*");
+            Console.WriteLine("*╚══╝╚╝╚╝╚══╝╚══╝╚══╝╚══╝╚╝╚╝╚═══╝╚══╝╚╝╚╝─╚╝╚╝*");
+            Console.WriteLine("************************************************");
+            Console.WriteLine("");
+            Console.WriteLine("Escoga su operación matemática");
+            Console.WriteLine("");
+            Console.WriteLine("1) Suma");
+            Console.WriteLine("2) Resta");
+            Console.WriteLine("3) Multiplicación");
+            Console.WriteLine("4) División");
+            Console.WriteLine("5) Salir");
+            palabra1 = Console.ReadLine();
+            numero1 = long.Parse(palabra1);
+            switch (numero1)
+            {
+                case 1:
+                    Console.WriteLine("SUMAS");
+                    Console.Write("Ingrese el primer número: ");
+                    palabra1 = Console.ReadLine();
+                    Console.Write("Ingrese el segundo número: ");
+                    palabra2 = Console.ReadLine();
+                    numero1 = long.Parse(palabra1);
+                    numero2 = long.Parse(palabra2);
+                    llamados.Sumas(numero1, numero2);
+                    Console.ReadKey(); Console.Clear(); Menucito();
+                    break;
+                case 2:
+                    Console.WriteLine("RESTAS");
+                    Console.Write("Ingrese el primer número: ");
+                    palabra1 = Console.ReadLine();
+                    Console.Write("Ingrese el segundo número: ");
+                    palabra2 = Console.ReadLine();
+                    numero1 = long.Parse(palabra1);
+                    numero2 = long.Parse(palabra2);
+                    llamados.Restas(numero1, numero2);
+                    Console.ReadKey(); Console.Clear(); Menucito();
+                    break;
+                case 3:
+                    Console.WriteLine("MULTIPLICACIÓN");
+                    Console.Write("Ingrese el primer número: ");
+                    palabra1 = Console.ReadLine();
+                    Console.Write("Ingrese el segundo número: ");
+                    palabra2 = Console.ReadLine();
+                    numero1 = long.Parse(palabra1);
+                    numero2 = long.Parse(palabra2);
+                    llamados.Multiplicacion(numero1, numero2);
+                    Console.ReadKey(); Console.Clear(); Menucito();
+                    break;
+                case 4:
+                    Console.WriteLine("DIVISIÓN");
+                    Console.Write("Ingrese el primer número: ");
+                    palabra1 = Console.ReadLine();
+                    Console.Write("Ingrese el segundo número: ");
+                    palabra2 = Console.ReadLine();
+                    numero1 = long.Parse(palabra1);
+                    numero2 = long.Parse(palabra2);
+                    llamados.Division(numero1, numero2);
+                    Console.ReadKey(); Console.Clear(); Menucito();
+                    break;
+                default:
+                    Console.WriteLine("Gracias por usar el aplicativo");
+                    Console.ReadKey(); Console.Clear();
+                    break;
+            }
+        }
     }
 }
